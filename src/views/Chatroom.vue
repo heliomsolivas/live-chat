009ffb4190e3af9bd149acb663cf181407ad9187
Hélio M S Olivas <heliomsolivas@gmail.com>
2022-05-06 16:26:06 -0300

    Logout watch

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -6,10 +6,22 @@
 </template>
 
 <script>
+import { watch } from "vue";
 import Navbar from "../components/Navbar.vue";
+import getUser from "../composables/getUser";
+import { useRouter } from "vue-router";
 export default {
   components: {
     Navbar,
+  },
+  setup() {
+    const { user } = getUser();
+    const router = useRouter();
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: "Welcome" });
+      }
+    });
   },
 };
 </script>
